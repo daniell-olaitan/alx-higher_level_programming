66a7dc247cf9ed2858a5c3eabce75d98a3226aa4
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-02 10:28:38 +0100

    remaining mandatory tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: the given list
+ *
+ * Return: 0 if there is no cycle, 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+
+	if (list == NULL)
+		return (0);
+	else if (list-next == NULL)
+		return (0);
+
+	tortoise = list->next;
+	hare = (list-next)->next;
+	while (hare)
+	{
+		if (tortoise == hare)
+			return (1);
+
+		tortoise = tortoise->next;
+		hare = hare->next;
+		if (hare == NULL)
+			break;
+
+		hare = hare->next;
+	}
+
+	return (0);
+}
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(f"{3*str:s}")
+print(f"{str[:9]:s}")
