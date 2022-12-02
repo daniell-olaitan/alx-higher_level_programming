51a6f1df84e9beb3c2ec693a38cf21a2a031ce1d
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-02 05:47:59 +0100

    corrected task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 import hidden_4
 if __name__ == '__main__':
     names = dir(hidden_4)
-    for e in hidden_4:
+    for e in names:
         if not e.startswith('__'):
             print("{}".format(e))
