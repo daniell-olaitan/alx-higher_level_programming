1d142b495ea02ebeca9d5a125be6481cacf999cd
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-22 14:09:00 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,6 +8,9 @@ def roman_to_int(roman_string):
     Return:
         the converted integer
     """
+    if type(roman_string) is not str or roman_string == None:
+        return 0
+
     digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = []
 
