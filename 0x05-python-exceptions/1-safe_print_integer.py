0fc65ff11573d75143f045eeab01e3d2ab517f77
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-23 12:21:27 +0100

    all mandatory tasks

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -12,5 +12,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
