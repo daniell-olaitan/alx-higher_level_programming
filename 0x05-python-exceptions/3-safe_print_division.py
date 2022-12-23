1a4fba95688bf9a3216aeadd2d408ce78390f4c8
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-23 12:36:31 +0100

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -14,4 +14,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {:f}".format(result))
