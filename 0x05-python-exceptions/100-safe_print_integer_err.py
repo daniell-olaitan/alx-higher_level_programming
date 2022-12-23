40fb4ec26e378b9092845901073deea790d2a3fd
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-23 13:43:16 +0100

    task 8 and 9

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer
+
+    Args:
+        value: the int to be printed
+
+    Return:
+        True: if the value has be correctly printed
+        False: if otherwise
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as exc:
+        sys.stderr.write(f"Exception: {exc}\n")
+        return False
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    """executes a function safely
+
+    Args:
+        fct: a pointer to the function to be executed
+        args: the arguments to the function
+
+    Returns:
+        The result of the function or None if exception occurs
+    """
+    try:
+        result = fct(*args)
+    except Exception as exc:
+        sys.stderr.write(f"Exception: {exc}\n")
+        return None
+
+    return result
