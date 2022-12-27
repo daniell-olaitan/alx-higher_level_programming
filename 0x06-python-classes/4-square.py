8cd6af384345dc49e10056a925d050de41e59ee5
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-27 13:05:29 +0100

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Contains a class that defines a square
+"""
+
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size: the length of the square
+    """
+    def __init__(self, size=0):
+        """initializes size
+
+        Args:
+            size: the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """computes the area of the square
+
+        Returns:
+            the computed area
+        """
+        return self.__size**2
