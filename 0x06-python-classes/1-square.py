531abd15458fc9ffc09a1ed69c0e0a6dee26deef
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-27 12:09:05 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Contains a class that defines a square
+"""
+
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size: the length of the square
+    """
+    def __init__(self, size):
+        """initializrs size
+
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
