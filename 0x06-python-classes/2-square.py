c69eb961ef22c35c0a46ba35ef48be47dbda9c61
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-27 12:30:17 +0100

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Contains a class that defines a square
+"""
+
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size: the length of the square
+    """
+    def __init__(self, size=0):
+        """initializes size
+
+        Args:
+            size: the size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
