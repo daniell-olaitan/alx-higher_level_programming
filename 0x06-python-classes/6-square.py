3cacefc615b9e8f19292854cc1c08f36e8631dd6
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-28 09:42:17 +0100

    task 6 correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,18 +73,3 @@ class Square:
                     print("#", end='')
 
                 print()
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
