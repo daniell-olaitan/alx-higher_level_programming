18523c875c99443bcaf991a4df9c396c26a43980
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-06 12:33:27 +0100

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Contains a function that adds two integers.
+"""
+def add_integer(a, b=98):
+    """Adds two integers
+
+    Args:
+        a: the first integer
+        b: second int (default value of 98)
+
+    Return:
+        the result of the addition
+    """
+    types = [int, float]
+    if (type(a) not in types) or (type(b) not in types):
+        if type(a) not in types:
+            raise TypeError("a must be an integer")
+        else:
+            raise TypeError("b must be an integer")
+
+    else:
+        return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Directory for all the tasks in python test driven development.
