7f0e6b424371f15068ecfa98be99ebefa9d4a574
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-07 17:19:10 +0100

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,12 +7,6 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class
-        Args:
-            width: represents the width of the rectangle
-            height: represents the height of the rectangle
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
         """
         self.width = width
         self.height = height
