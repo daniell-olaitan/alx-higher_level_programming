e8c49dd0549febd22506be94a0404b86603fb7b4
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-07 17:22:38 +0100

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,7 +18,6 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """sets width attribute"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
