e8f15b2be05fe186b5bb567485948c7c3e8ab1af
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-07 16:08:52 +0100

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Contains a class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """defines a rectangle
+    """
+    @property
+    def width(self):
+        """getter retrieves __width
+           setter sets __width to int and >= 0
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(width) not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter retrieves __height
+           setter sets __height to int and >= 0
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(height) not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        """initialises __width and __height
+        """
+        self.width = width
+        self.height = height
---
+++ 0x08-python-more_classes/1-rectangle.py~
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains a class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """defines a rectangle
+    """
+    pass
