063c7114d1283db4bde078c7b8950d4f36c5f3eb
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-09 23:10:09 +0100

    madatory tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""contains a function that lists all the attr of an object
-"""
-
-
 def lookup(obj):
-    """returns the list of available attributes and method of an obj
-
-    Args:
-        obj: the given object
     """
+    Returns the list of available attributes and methods of an object
+    """
+
     return dir(obj)
