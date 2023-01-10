cdec9a7a9f9bbb9523f7c8d052a6efcf30565fae
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-10 19:54:56 +0100

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""contains a function that writes a string to a file"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+
+    Args:
+        filename: name of file
+        text: string to write to file
+
+    Return:
+        number of characters written to file
+    """
+    if filename:
+        with open(filename, 'w', encoding='utf-8') as writeFile:
+            writeFile.write(text)
