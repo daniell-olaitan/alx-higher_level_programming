c77b8cedac93a3de19abc352ecf3a171fd9eb9c2
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-11 14:32:43 +0100

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 """contains a function that writes an obj's json rep to a text file"""
-
 import json
 
 
 def save_from_json_file(my_obj, filename):
-    """writes an Object to a text file
-
-    Args:
-        my_obj: the object to write to file
-        filename: the file to write to
-    """
-
+    """writes an Object to a text file"""
     if filename:
         with open(filename, 'w', encoding="utf-8") as fd:
             json.dump(my_obj, fd)
