6de67b5e53733f1c57b06a8c6a95827bc9f508e7
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-12 00:34:13 +0100

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""modules adds args to a list and save them to a filee"""
+import json
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+res = load_from_json_file(filename)
+save_to_json_file(res + argv[1:], filename)
