1e30399bd7eb9b1f1b720fd9b525196625a6c31a
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-12 01:07:09 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     except FileNotFoundError:
         res = []
 
-    save_to_json_file(res.extend(sys.argv[1:]), filename)
+    save_to_json_file(res + sys.argv[1:], filename)
