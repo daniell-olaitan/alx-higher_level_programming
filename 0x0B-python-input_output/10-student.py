ca7dbc79ef7f20afbae25f026b4b85e394f27335
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-12 02:12:31 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""contains a class that defines a student"""
+
+
+class Student:
+    """a class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dict representation of a Student instance"""
+        var = vars(self)
+        if attrs:
+            res = {}
+            for key in attrs:
+                res[key] = var[key]
+
+            return res
+
+        return vars(sef)
