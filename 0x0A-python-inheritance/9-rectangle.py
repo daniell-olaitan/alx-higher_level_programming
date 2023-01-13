e5272e8c1d3467bf60eaa9926ad78d2b53fc591c
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-13 13:23:15 +0100

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defines geometry"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defines rectangle geometry"""
+
+    def __init__(self, width, height):
+        """instantiates and validates width and height of the rectangle"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """computes and returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
