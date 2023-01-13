90c106ce98dfdf764469f6083824affbc4917fc6
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-13 12:34:35 +0100

    task 4 c

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""contains a func that determines if an obj's class inherits from a class"""
+"""contains a func that determines if an objs class inherits from a class"""
 
 
-def inherites_from(obj, a_cass):
-    """determines if an obj's class inherites from a class"""
+def inherits_from(obj, a_cass):
+    """determines if an objs class inherites from a class"""
     return issubclass(type(obj), a_class)
