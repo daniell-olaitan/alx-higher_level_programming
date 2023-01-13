b478e19fbbf808e542811a7f0709fc5511fdd4ae
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-13 13:35:02 +0100

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""implements square class from rectangle class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """defines square"""
+
+    def __init__(self, size):
+        """initializes size and validates it size"""
+        super().__init__(size, size)
+        self.__size = size
