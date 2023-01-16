f3ba8d3e833e1d856f3f6960279c1f862e7d1369
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-16 19:02:48 +0100

    task 2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,5 +11,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            super().__nb_objects += 1
+            self.id = super().__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""contains a class that defines Rectange"""
+from base import Base
+
+
+class Rectangle(Base):
+    """defines a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """gets and sets the attr"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+          self.__y = value
+
+    @property
+    def y(self):
+        return self.__y
