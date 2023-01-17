41eaf25a28ad2e8f6ad6a3ef1147b2f00d42f0fa
daniell-olaitan <daniell.olaitan@gmail.com>
2023-01-17 17:56:42 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,4 +10,4 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
