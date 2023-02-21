f744c2c5ca3c07b052de4dc978e04bfb1b6735fd
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-21 08:06:51 -0800

    attempt

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all the records of a table
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the number of records with id = 89
+SELECT COUNT(id)
+FROM first_table
+WHERE id = 89;
