9fc1db2fa27e74179131f900fccee622face032c
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-23 09:31:23 +0100

    attempt

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates the MySQL server user_0d_1
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
