8ac1090d84e16b7dda096cdb24a1f94023b0b09b
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-23 04:58:27 -0800

    attempt

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,6 @@
 -- lists all shows contained in a database that has at lease one genre link
 
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+     INNER JOIN tv_show_genres AS tsg
+     ON ts.id = tsg.show_id;
