cc13df4f971c11ce59c326fe44abedd391ab6f68
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-23 05:01:54 -0800

    attempt

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,5 @@
 SELECT ts.title, tsg.genre_id
 FROM tv_shows AS ts
      INNER JOIN tv_show_genres AS tsg
-     ON ts.id = tsg.show_id;
+     ON ts.id = tsg.show_id
+ORDER BY ts.title ASC, tsg.genre_id ASC;
