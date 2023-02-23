d0742acb466e5d944d8f60b295cb8c5d532f30fe
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-23 05:45:19 -0800

    attempt

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all genres in the db and the number of shows linked to each
+
+SELECT tg.name as genre, COUNT(tsg.genre_id) AS number_of_shows
+FROM tv_genres AS tg
+     INNER JOIN tv_show_genres AS tsg
+     ON tg.id = tsg.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
