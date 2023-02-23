82066bbc11888122cac8f752a810fa6203640093
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-23 07:57:22 -0800

    attempt

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- lists all comedy shows in the db
+
+SELECT ts.title
+FROM tv_shows AS ts
+     INNER JOIN tv_show_genres AS tsg
+     	   ON ts.id = tsg.show_id
+     INNER JOIN tv_genres AS tg
+     	   ON tsg.genre_id = tg.id
+WHERE tg.name = "Comedy"
+ORDER BY ts.title;
