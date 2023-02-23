342aaaee73fc7fbb99e3aaae2d328194bd229f99
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-23 05:18:18 -0800

    attempt

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,6 @@
 SELECT ts.title, tsg.genre_id
 FROM tv_shows AS ts
      LEFT OUTER JOIN tv_show_genres AS tsg
-     ON tsg.show_id IS NULL
+     ON ts.id = tsg.show_id
+WHERE tsg.show_id IS NULL
 ORDER BY ts.title, tsg.genre_id;
