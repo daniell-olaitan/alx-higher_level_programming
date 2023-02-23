add4cc4acffc5ba334cccb3499f0c0f9bfff3741
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-23 10:33:29 +0100

    attempt

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       FOREIGN KEY (stat_id) REFERENCES states(id)
+       FOREIGN KEY (state_id) REFERENCES states(id)
 );
