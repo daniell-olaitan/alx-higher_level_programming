a62fb06a0a029d0894a936e6e13591f2b3c7c654
daniell-olaitan <daniell.olaitan@gmail.com>
2023-02-24 08:59:17 +0100

    attempt

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,9 +2,9 @@
 
 SELECT DISTINCT ts.title
 FROM tv_shows AS ts
-     INNER JOIN tv_show_genres AS tsg
+     LEFT JOIN tv_show_genres AS tsg
      	   ON ts.id = tsg.show_id
-     INNER JOIN tv_genres AS tg
+     LEFT JOIN tv_genres AS tg
      	   ON tg.id = tsg.genre_id
 WHERE ts.title NOT IN (
       SELECT ts.title
