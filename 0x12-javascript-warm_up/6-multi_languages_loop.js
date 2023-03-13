6a30d56caab2d467b5da9b1dda10d94de28d6127
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-13 18:40:11 +0100

    attempt

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,8 @@
 
 const langs = ['C is fun',
 	       'Python is cool',
-	       'JavaScript is amazing'
-	      ];
+	       'JavaScript is amazing'];
 
 for (let i = 0; i < langs.length; ++i) {
-    console.log(langs[i]);
+  console.log(langs[i]);
 }
