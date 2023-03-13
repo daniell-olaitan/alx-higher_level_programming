90e355c58611eb799c41f401489727c684643eb5
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-13 18:49:25 +0100

    attempt

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const x = parseInt(process.argv[2]);
 const str = 'C is fun';
 
 if (x) {
-    for (let i = 0; i < x; ++i) {
-	console.log(str);
-    }
+  for (let i = 0; i < x; ++i) {
+    console.log(str);
+  }
 } else {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
