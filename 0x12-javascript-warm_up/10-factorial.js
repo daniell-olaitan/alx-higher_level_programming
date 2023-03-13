2ecc8948acb9be875dd89b24b7576a89686434f8
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-13 18:44:48 +0100

    attempt

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
 function factorial (n) {
-    if (n === 0 || isNaN(n)) return 1;
+  if (n === 0 || isNaN(n)) {
+	return 1;
+  }
 
-    return (n * factorial(n - 1));
+  return (n * factorial(n - 1));
 }
 
 const n = parseInt(process.argv[2]);
