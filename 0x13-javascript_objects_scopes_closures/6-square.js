666058916f998579f17a33033049ad3a3146c6ae
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-14 17:30:19 +0100

    attempt

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const OldSquare = require('./5-square');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends OldSquare {
   constructor (size) {
     super(size, size);
   }
