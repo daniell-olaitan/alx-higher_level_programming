dd3e1264170216008144d39bb57507b73f6f333a
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-14 17:26:36 +0100

    attempt

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,10 +8,10 @@ module.exports = class Rectangle {
     }
   }
 
-  print () {
+  print (c = 'X') {
     for (let i = 0; i < this.height; ++i) {
       for (let j = 0; j < this.width; ++j) {
-        process.stdout.write('X');
+        process.stdout.write(c);
       }
 
       console.log('');
