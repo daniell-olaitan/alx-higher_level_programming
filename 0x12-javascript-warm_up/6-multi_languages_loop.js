15d635433d52622aa257ebf5951c03d2112181bb
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-14 02:40:48 +0100

    attempt

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < languages.length; ++i) {
   console.log(languages[i]);
