7406fdb46f70cf9deb0fdf20245bdb99e49ceed6
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-15 01:27:06 +0100

    attempt

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,11 +3,11 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
-Object.keys(dict).map(key => (
+Object.keys(dict).map(key => {
   if (!Array.isArray(newDict[dict[key]])) {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
-));
+});
 
 console.log(newDict);
