7c4ba74e7b2044423bbfce68d269be5a0afad6c1
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-25 01:05:26 +0100

    Changes the number of column from the output table and adds clean up codes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], database=argv[3])
     cur = db.cursor()
-    query = "SELECT cities.id, cities.name, states.name \
+    query = "SELECT cities.id, cities.name \
              FROM cities \
              JOIN states \
              ON cities.state_id = states.id \
