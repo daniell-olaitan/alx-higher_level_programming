8de50513ea4baff91c662e86ec4e7d74cb7adbbf
daniell-olaitan <daniell.olaitan@gmail.com>
2023-04-18 19:09:27 +0100

    All the file should be 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends request to that URL, and displays the size of the
-# body of the response
-
+# Takes in a URL, sends request to that URL, and displays the size of the body
 curl -s -w "%{size_download}\n" -o "/dev/null" "$1"
