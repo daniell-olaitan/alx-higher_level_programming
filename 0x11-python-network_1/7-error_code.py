a65e602a69101d7da6bcb46b1c36c476d2ce0b41
daniell-olaitan <daniell.olaitan@gmail.com>
2023-04-20 18:49:20 -0700

    Corrected a logical error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ import requests
 
 if __name__ == '__main__':
     res = requests.get(argv[1])
-    if res.status_code >= 40:
+    if res.status_code >= 400:
         print('Error code:', res.status_code)
     else:
         print(res.text)
