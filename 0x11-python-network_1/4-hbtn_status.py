5cf8c1d4b278f740f74fa7cbabe2c378e1f93cf0
daniell-olaitan <daniell.olaitan@gmail.com>
2023-04-20 17:09:42 -0700

    Get requesdt using requests library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,3 +4,11 @@ Fetches a url using requests library
 """
 
 import requests
+
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print('Body response:')
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
