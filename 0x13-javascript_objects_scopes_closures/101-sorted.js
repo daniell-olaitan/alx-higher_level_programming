337c4a50683297d4a34fd58ea03cceb3c97f94bf
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-28 05:28:50 +0100

    Fix a semistandard rule

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,11 +3,15 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
-Object.keys(dict).map(function (key) {
+const func = function (key) {
   if (!Array.isArray(newDict[dict[key]])) {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
-});
+};
+
+for (const key of Object.keys(dict)) {
+  func(key);
+}
 
 console.log(newDict);
