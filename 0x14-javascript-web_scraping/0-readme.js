62f57a956e137ae5287471e5bd8a96a006ed0704
daniell-olaitan <daniell.olaitan@gmail.com>
2024-01-28 23:14:30 +0100

    Implement task 1 of the project

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (error, data) => {
   if (error) console.log(error);
   else console.log(data);
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error) => {
+  if (error) console.log(error);
+});
