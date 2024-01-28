4ce2e25931c3e2545ccee7063f85ffc6f4919b7f
daniell-olaitan <daniell.olaitan@gmail.com>
2024-01-28 23:06:48 +0100

    Complete task 0 of the project

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const fs = require('fs')
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  if (error) console.log(error);
+  else console.log(data);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+Directory contains all the tasks in this project.
