2d933be85da06bda9fbbbd0f1b723a93c7871600
daniell-olaitan <daniell.olaitan@gmail.com>
2024-01-28 23:55:20 +0100

    Implemet task 3 of the project

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-let filmUrl = 'https://swapi-api.alx-tools.com/api/films' + process.argv[2];
+const filmUrl = 'https://swapi-api.alx-tools.com/api/films' + process.argv[2];
 request(filmUrl, (error, response, body) => {
   if (error) console.log(error);
-  else console.log(JSON.parse(body).title);
+  else console.log(body.title);
 });
