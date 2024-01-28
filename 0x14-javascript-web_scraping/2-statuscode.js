ca801642dadebbb486b80c5fa407fe926b57bd5f
daniell-olaitan <daniell.olaitan@gmail.com>
2024-01-28 23:26:28 +0100

    Implement task 2 of the project

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) console.log(error);
+  else console.log(`code: ${response.statusCode}`);
+});
