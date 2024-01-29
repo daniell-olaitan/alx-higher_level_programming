87672d87ff57862d8c5a81232ac48e6855f41efe
daniell-olaitan <daniell.olaitan@gmail.com>
2024-01-29 11:35:10 +0100

    Implement task 101 of the project

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (error) console.log(error);
   else {
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, (error, response, body) => {
-  if (error) console.log(error);
-  else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, (error, response, body) => {
-        if (error) console.log(error);
-        else console.log(JSON.parse(body).name);
-      });
-    }
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
